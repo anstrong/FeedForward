54ea1fdfea912370d1ec17d5ec10177491accca6
Post Man <post@man.com>
2023-10-11 14:14:29 -0400

    Server and Dao In Progress

--- server/server.js
+++ server/server.js
@@ -1,3 +1,52 @@
+const express = require("express");
+const dao = require("./dao.js");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+
+function queryDAO(operation, req, res) {
+    dao.call(operation, req.params, (result) => {
+      if (result.body && result.status != 404) {
+        console.log(result.body);
+        res.send(result.body);
+      } else {
+        res.sendStatus(404);
+        res.end;
+      }
+    });
+  }
+
+
+app.post('/login', (req, res) => {
+    queryDAO('authenticate', req, res);
+});
+
+app.get("/users/:user/entries", (req, res) => {
+    queryDAO('get_entries', req, res);
+});
+
+app.get("/users/:user", (req, res) => {
+    queryDAO('get_reports', req, res);
+});
+
+app.get("/users/:user/direct_reports", (req, res) => {
+    queryDAO('get_reports', req, res);
+});
+
+app.post("/:sender/:recipient/:body", (req, res) => {
+    queryDAO('post_entry', req, res);
+});
+
+const port = 3000;
+console.log("server starting on port " + port);
+app.listen(port);
+
+
+
+
+/*
 const express = require('express');
 const dao = require('./dao.js');
 const bodyParser = require('body-parser');
@@ -37,3 +86,4 @@ app.post("/:sender/:recipient/:body", (req, res) => {
 const port = 3000;
 console.log('server starting on port ' + port);
 app.listen(port);
+*/
