cb30732c9b2890bf1e96d322c54b7cc45998ac6a
Post Man <post@man.com>
2023-10-11 15:03:47 -0400

    Server and Dao Working

--- server/dao.js
+++ server/dao.js
@@ -2,9 +2,9 @@ const { MongoClient } = require('mongodb');
 
 const uri = "mongodb+srv://admin:admin@feedforwarddb.d3pydak.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
-const db_name = "feedforwarddb";
+const db_name = "data";
 
-module.exports.call = async function call(operation, params, callback) {
+module.exports.call = async function call(operation, req, callback) {
     const db = client.db(db_name);
 
     switch (operation.toLowerCase()) {
@@ -12,8 +12,8 @@ module.exports.call = async function call(operation, params, callback) {
         case 'authenticate':
             try{
                 const collection = db.collection("users")
-                const username = params.body.username;
-                const password = params.body.password;
+                const username = req.body.username;
+                const password = req.body.password;
                 const user = await collection.findOne({ username: username });
                
                 if (user && user.password === password) {
@@ -21,7 +21,6 @@ module.exports.call = async function call(operation, params, callback) {
                 } else {
                     callback({ body: "Invalid credentials", status: 401 });
                 }
-                
             } catch (e) {
                 console.log(e);
                 callback({ body: false, status: 404});
@@ -32,9 +31,9 @@ module.exports.call = async function call(operation, params, callback) {
         case 'get_user':
             try{
                 const collection = db.collection("users")
-                const username = params.body.username;
+                const username = req.params.username;
                 const user = await collection.findOne({ username: username });
-                callback({ body: { user }, status: 200 })
+                callback({ body: user, status: 200 })
             } catch (e) {
                 console.log(e);
                 callback({ body: false, status: 404});
@@ -45,9 +44,9 @@ module.exports.call = async function call(operation, params, callback) {
         case 'get_entries':
             try{
                 const collection = db.collection("entries")
-                const username = params.body.username;
-                const entries = await collection.find({ recipient: username });
-                callback({ body: { entries }, status: 200 })
+                const username = req.params.username;
+                const entries = await collection.find({ recipient: username }).toArray();
+                callback({ body: entries, status: 200 })
             } catch (e) {
                 console.log(e);
                 callback({ body: false, status: 404});
@@ -55,6 +54,7 @@ module.exports.call = async function call(operation, params, callback) {
                 break;
             }
         
+       
                
         default:
             callback({ status: "requested data not found", body: params });
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 
 
 function queryDAO(operation, req, res) {
-    dao.call(operation, req.params, (result) => {
+    dao.call(operation, req, (result) => {
       if (result.body && result.status != 404) {
         console.log(result.body);
         res.send(result.body);
@@ -20,24 +20,27 @@ function queryDAO(operation, req, res) {
 
 
 app.post('/login', (req, res) => {
+    console.log("We found Post Login")
     queryDAO('authenticate', req, res);
 });
 
-app.get("/users/:user/entries", (req, res) => {
+app.get("/users/:username/entries", (req, res) => {
     queryDAO('get_entries', req, res);
 });
 
-app.get("/users/:user", (req, res) => {
+app.get("/users/:username", (req, res) => {
+    queryDAO('get_user', req, res);
+});
+
+app.get("/users/:username/direct_reports", (req, res) => {
     queryDAO('get_reports', req, res);
 });
 
-app.get("/users/:user/direct_reports", (req, res) => {
-    queryDAO('get_reports', req, res);
-});
-
-app.post("/:sender/:recipient/:body", (req, res) => {
+app.post("/send", (req, res) => {
     queryDAO('post_entry', req, res);
 });
+
+
 
 const port = 3000;
 console.log("server starting on port " + port);
