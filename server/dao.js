372466842caba9da50dd62e7f24a7fdc3e811fd9
Post Man <post@man.com>
2023-10-11 16:51:43 -0400

    Backend Routes Completed

--- server/dao.js
+++ server/dao.js
@@ -1,67 +1,87 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb");
 
-const uri = "mongodb+srv://admin:admin@feedforwarddb.d3pydak.mongodb.net/?retryWrites=true&w=majority";
+
+const uri =
+  "mongodb+srv://admin:admin@feedforwarddb.d3pydak.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 const db_name = "data";
 
 module.exports.call = async function call(operation, req, callback) {
-    const db = client.db(db_name);
+  const db = client.db(db_name);
 
-    switch (operation.toLowerCase()) {
+  switch (operation.toLowerCase()) {
+    case "authenticate":
+      try {
+        const collection = db.collection("users");
+        const username = req.body.username;
+        const password = req.body.password;
+        const user = await collection.findOne({ username: username });
 
-        case 'authenticate':
-            try{
-                const collection = db.collection("users")
-                const username = req.body.username;
-                const password = req.body.password;
-                const user = await collection.findOne({ username: username });
-               
-                if (user && user.password === password) {
-                    callback({ body: { username }, status: 200 })
-                } else {
-                    callback({ body: "Invalid credentials", status: 401 });
-                }
-            } catch (e) {
-                console.log(e);
-                callback({ body: false, status: 404});
-            } finally {
-                break;
-            }
-        
-        case 'get_user':
-            try{
-                const collection = db.collection("users")
-                const username = req.params.username;
-                const user = await collection.findOne({ username: username });
-                callback({ body: user, status: 200 })
-            } catch (e) {
-                console.log(e);
-                callback({ body: false, status: 404});
-            } finally {
-                break;
-            }
-            
-        case 'get_entries':
-            try{
-                const collection = db.collection("entries")
-                const username = req.params.username;
-                const entries = await collection.find({ recipient: username }).toArray();
-                callback({ body: entries, status: 200 })
-            } catch (e) {
-                console.log(e);
-                callback({ body: false, status: 404});
-            } finally {
-                break;
-            }
-        
-       
-               
-        default:
-            callback({ status: "requested data not found", body: params });
-            break;
-    };
+        if (user && user.password === password) {
+          callback({ body: { username }, status: 200 });
+        } else {
+          callback({ body: "Invalid credentials", status: 401 });
+        }
+      } catch (e) {
+        console.log(e);
+        callback({ body: false, status: 404 });
+      } finally {
+        break;
+      }
 
-    console.log('call complete: ' + operation);
-    // client.close();
-    return 'call complete';
+    case "get_user":
+      try {
+        const collection = db.collection("users");
+        const username = req.params.username;
+        const user = await collection.findOne({ username: username });
+        callback({ body: user, status: 200 });
+      } catch (e) {
+        console.log(e);
+        callback({ body: false, status: 404 });
+      } finally {
+        break;
+      }
+
+    case "get_entries":
+      try {
+        const collection = db.collection("entries");
+        const username = req.params.username;
+        const entries = await collection
+          .find({ recipient: username })
+          .toArray();
+        callback({ body: entries, status: 200 });
+      } catch (e) {
+        console.log(e);
+        callback({ body: false, status: 404 });
+      } finally {
+        break;
+      }
+
+    case "post_entry":
+      try {
+        const collection = db.collection("entries");
+        const document = {
+          body: req.body.body,
+          sender: req.body.sender,
+          recipient: req.body.recipient,
+          valence: req.body.valence
+        };
+        const body = await collection.insertOne(document);
+        // console.log(`Entry ID #: ${body.insertedId}`);
+        callback({ body: body, status: 200 });
+      } catch (e) {
+        console.log(e);
+        callback({ body: false, status: 404 });
+      } finally {
+        break;
+    }
+
+    default:
+      callback({ status: "requested data not found", body: params });
+      break;
+  }
+
+  console.log("call complete: " + operation);
+  // client.close();
+  return "call complete";
 };
--- server/server.js
+++ server/server.js
@@ -41,52 +41,6 @@ app.post("/send", (req, res) => {
 });
 
 
-
 const port = 3000;
 console.log("server starting on port " + port);
 app.listen(port);
-
-
-
-
-/*
-const express = require('express');
-const dao = require('./dao.js');
-const bodyParser = require('body-parser');
-
-const app = express();
-app.use(bodyParser.json());
-
-function queryDAO(operation, req, res) {
-    dao.call(operation, req.params, (result) => {
-        if (result.body && result.status != 404) {
-            console.log(result.body);
-            res.send(result.body);
-        } else {
-            res.sendStatus(404);
-            res.end;
-        };
-    });
-}
-
-app.get("/authenticate/:user/:pass", (req, res) => {
-    queryDAO('check_user', req, res);
-});
-
-app.get("/:user/entries", (req, res) => {
-    queryDAO('get_entries', req, res);
-});
-
-app.get("/:user/direct_reports", (req, res) => {
-    queryDAO('get_reports', req, res);
-});
-
-app.post("/:sender/:recipient/:body", (req, res) => {
-    queryDAO('post_entry', req, res);
-});
-
-
-const port = 3000;
-console.log('server starting on port ' + port);
-app.listen(port);
-*/
