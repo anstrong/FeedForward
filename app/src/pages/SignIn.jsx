b210a8a7140d69a9eb5d93cf49e1102168f0dd79
anstrong <anstrong@users.noreply.github.com>
2023-10-11 16:07:01 -0400

    Working login

--- app/src/pages/SignIn.jsx
+++ app/src/pages/SignIn.jsx
@@ -6,22 +6,36 @@ function SignIn({ onSuccessfulAuth }) {
 
     function handleSubmit(event) {
         event.preventDefault();
-        const uid = authenticate(username, password);
-        if (!uid) {
-            alert("Credentials are not valid");
-            setUsername("");
-            setPassword("");
-        } else {
-            onSuccessfulAuth(uid)
-        }
+        authenticate(username, password);
+    }
+
+    function handleFailure(e) {
+        console.log(e)
+        alert("Credentials are not valid");
+        setUsername("");
+        setPassword("");
     }
 
     function authenticate(username, password) {
-        return '123456789'
-        /*fetch('localhost:3000/login', {
-            method: "POST",
-            body: { username: username, password: password }
-        }).then(response => console.log(response))*/
+        try {
+            fetch('http://localhost:8080/login/', {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username: username, password: password })
+            }).then(response => {
+                console.log(response);
+                return response.json();
+            }).then(payload => {
+                console.log(payload);
+                let uid = payload['username'];
+                onSuccessfulAuth(uid);
+            }).catch(e => handleFailure(e));
+        } catch (e) {
+            handleFailure(e)
+        }
     }
 
     return (
--- server/server.js
+++ server/server.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const dao = require("./dao.js");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use(cors());
+app.use(express.json());
 
 function queryDAO(operation, req, res) {
     dao.call(operation, req, (result) => {
-      if (result.body && result.status != 404) {
-        console.log(result.body);
+      if (result.body && result.status < 400) {
         res.send(result.body);
+        res.sendStatus(result.status);
       } else {
-        res.sendStatus(404);
+        console.log(req.body)
+        res.sendStatus(result.status);
         res.end;
       }
     });
@@ -20,7 +23,6 @@ function queryDAO(operation, req, res) {
 
 
 app.post('/login', (req, res) => {
-    console.log("We found Post Login")
     queryDAO('authenticate', req, res);
 });
 
@@ -42,51 +44,6 @@ app.post("/send", (req, res) => {
 
 
 
-const port = 3000;
+const port = 8080;
 console.log("server starting on port " + port);
 app.listen(port);
-
-
-
-
-/*
-const express = require('express');
-const dao = require('./dao.js');
-const bodyParser = require('body-parser');
-
-const app = express();
-app.use(bodyParser.json());
-
-function queryDAO(operation, req, res) {
-    dao.call(operation, req.params, (result) => {
-        if (result.body && result.status != 404) {
-            console.log(result.body);
-            res.send(result.body);
-        } else {
-            res.sendStatus(404);
-            res.end;
-        };
-    });
-}
-
-app.get("/authenticate/:user/:pass", (req, res) => {
-    queryDAO('check_user', req, res);
-});
-
-app.get("/:user/entries", (req, res) => {
-    queryDAO('get_entries', req, res);
-});
-
-app.get("/:user/direct_reports", (req, res) => {
-    queryDAO('get_reports', req, res);
-});
-
-app.post("/:sender/:recipient/:body", (req, res) => {
-    queryDAO('post_entry', req, res);
-});
-
-
-const port = 3000;
-console.log('server starting on port ' + port);
-app.listen(port);
-*/
