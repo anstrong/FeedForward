dddb685dd54de2edcddc5c4048896b3e7ceac209
anstrong <anstrong@users.noreply.github.com>
2023-10-12 20:51:04 -0400

    Text overflow support in entries

--- app/src/components/Entry.jsx
+++ app/src/components/Entry.jsx
@@ -1,11 +1,11 @@
 export default function Entry(params) {
-    const truncate = 75;
+    const truncate = 1000;
 
     return <div className="entry">
         <div class='sender'>
             <h3>{params.sender}</h3>
         </div>
-        <div>
+        <div class="text">
             <p>{params.body.length > truncate? params.body.slice(0, 75) + '...' : params.body}</p>
         </div>
     </div>;
